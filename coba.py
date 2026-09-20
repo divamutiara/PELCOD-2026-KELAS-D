424a4d1d04f5518d55b66cd481da363ea10953bf
moch. dias gustarino <divamtraaa@gmail.com>
2026-09-20 10:11:57 +0700

    first bgt

---
+++ coba.py
@@ -0,0 +1,9 @@
+jumlah_tiket="4"
+harga_tiket=25000
+
+total_sementara=jumlah_tiket *2
+print("Total sementara :", total_sementara)
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket*harga_tiket
+print("total akhir :", total_akhir)
